a99f75eed4d9014eae3113b5897d05df3896298f
Frank Reyes Martinez <frank_reyesm@hotmail.com>
2020-01-08 00:27:09 -0600

    delete a row tag

--- resources/views/pages/empresa.blade.php
+++ resources/views/pages/empresa.blade.php
@@ -5,38 +5,36 @@
     <div id="colorlib-project">
         <div class="container">
             <div class="row">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h3>QUIÉNES SOMOS</h3>
-                        <hr>
-                        <p>
-                            SOMOS UNA EMPRESA JOVEN DEDICADA AL DISEÑO, CONSTRUCCION,
-                            SOPORTE TECNICO, MANTENIMIENTO Y SUMINISTROS DE ESTACIONES DE
-                            SERVICIO TIPO GASOLINERA CON PERSONAL  DE EXPERIENCIA
-                            ALTAMENTE CALIFICADOS Y TECNICOS CERTIFICADOS.
-                        </p>
-                    </div>
-                    <div class="col-md-12" align="center">
-                        <img src="{{asset('images/team.png')}}" style="padding: 15px 0 25px 0; width: 50%;" align="center">
-                    </div>
-                    <div class="col-md-12">
-                        <h3>MISIÓN</h3>
-                        <hr>
-                        <p>
-                            ATENDER A NUESTROS CLIENTES, CON TIEMPO OPORTUNO,
-                            OFRECIENDO CALIDAD EN EL SERVICIO Y PRECIOS JUSTOS, ACTUANDO
-                            CON  ÉTICA Y RESPONSABILIDAD.
-                        </p>
-                        <br>
-                        <h3>VISIÓN</h3>
-                        <hr>
-                        <p>
-                            SER UNA EMPRESA LIDER EN SERVICIOS DE CALIDAD, MEJORANDO LOS
-                            ESTANDARES ESTABLECIDOS, ATRAVES DE LA INNOVACION DE
-                            TECNOLOGIAS Y CAPACITACIONES CONSTANTES.
-                        </p>
-                        <br>
-                    </div>
+                <div class="col-md-12">
+                    <h3>QUIÉNES SOMOS</h3>
+                    <hr>
+                    <p>
+                        SOMOS UNA EMPRESA JOVEN DEDICADA AL DISEÑO, CONSTRUCCION,
+                        SOPORTE TECNICO, MANTENIMIENTO Y SUMINISTROS DE ESTACIONES DE
+                        SERVICIO TIPO GASOLINERA CON PERSONAL  DE EXPERIENCIA
+                        ALTAMENTE CALIFICADOS Y TECNICOS CERTIFICADOS.
+                    </p>
+                </div>
+                <div class="col-md-12" align="center">
+                    <img src="{{asset('images/team.png')}}" style="padding: 15px 0 25px 0; width: 50%;" align="center">
+                </div>
+                <div class="col-md-12">
+                    <h3>MISIÓN</h3>
+                    <hr>
+                    <p>
+                        ATENDER A NUESTROS CLIENTES, CON TIEMPO OPORTUNO,
+                        OFRECIENDO CALIDAD EN EL SERVICIO Y PRECIOS JUSTOS, ACTUANDO
+                        CON  ÉTICA Y RESPONSABILIDAD.
+                    </p>
+                    <br>
+                    <h3>VISIÓN</h3>
+                    <hr>
+                    <p>
+                        SER UNA EMPRESA LIDER EN SERVICIOS DE CALIDAD, MEJORANDO LOS
+                        ESTANDARES ESTABLECIDOS, ATRAVES DE LA INNOVACION DE
+                        TECNOLOGIAS Y CAPACITACIONES CONSTANTES.
+                    </p>
+                    <br>
                 </div>
             </div>
         </div>
