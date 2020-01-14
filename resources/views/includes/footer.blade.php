2a856ff318cb704d2b5d0236b975a0e8684e261f
Frank Reyes Martinez <frank_reyesm@hotmail.com>
2020-01-13 22:58:10 -0600

    change the logo an update footer information

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="row row-pb-md">
         <div class="col-md-4 colorlib-widget">
             <h4>Acerca de la compañia</h4>
-            <p>Far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics</p>
+            <p>Somos una empresa joven dedicada al diseño, construcción, soporte técnico, mantenimento y suministros de estaciones de servicio tipo gasolinera con personal de experiencia altamente calificados y técnicos certificados.</p>
             <p>
             <ul class="colorlib-social-icons">
                 <li><a href="#"><i class="icon-twitter"></i></a></li>
@@ -16,13 +16,9 @@
             <h4>Información</h4>
             <p>
             <ul class="colorlib-footer-links">
-                <li><a href="#"><i class="icon-check"></i> Our Company</a></li>
-                <li><a href="#"><i class="icon-check"></i> Certification</a></li>
-                <li><a href="#"><i class="icon-check"></i> Our services</a></li>
-                <li><a href="#"><i class="icon-check"></i> Career</a></li>
-                <li><a href="#"><i class="icon-check"></i> Core Values</a></li>
-                <li><a href="#"><i class="icon-check"></i> Company History</a></li>
-                <li><a href="#"><i class="icon-check"></i> FAQ</a></li>
+                <li><a href="{{route('goarv.company')}}"><i class="icon-check"></i> Nuestra empresa</a></li>
+                <li><a href="#"><i class="icon-check"></i> Nuestros Servicios</a></li>
+                <li><a href="#"><i class="icon-check"></i> Nuestro Equipo</a></li>
             </ul>
             </p>
         </div>
@@ -30,10 +26,10 @@
         <div class="col-md-4 col-md-push-1">
             <h4>Información de contacto</h4>
             <ul class="colorlib-footer-links">
-                <li>291 South 21th Street, <br> Suite 721 New York NY 10016</li>
-                <li><a href="tel://1234567920"><i class="icon-phone"></i> + 1235 2355 98</a></li>
-                <li><a href="mailto:info@yoursite.com"><i class="icon-envelope"></i> info@yoursite.com</a></li>
-                <li><a href="http://luxehotel.com"><i class="icon-location4"></i> yourwebsite.com</a></li>
+                <li>Col. Rancho Alegre II, <br> Privada de Laureles 20-B, <br> CP. 96558, <br> COATZACOALCOS VER</li>
+                <li><a href="tel://1234567920"><i class="icon-phone"></i> 921 16 34 393</a></li>
+                <li><a href="mailto:info@yoursite.com"><i class="icon-envelope"></i> info@goarv.com.mx</a></li>
+                <li><a href="http://luxehotel.com"><i class="icon-location4"></i> www.goarv.com.mx</a></li>
             </ul>
         </div>
     </div>
