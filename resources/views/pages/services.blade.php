10ac447cbd66c41a8b9628975ec12d5f806cf6ec
Frank Reyes Martinez <frank_reyesm@hotmail.com>
2020-01-15 00:05:52 -0600

    work in services and team

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -8,6 +8,63 @@
                 <h3>NUESTROS SERVICIOS</h3>
                 <hr>
                 <br>
+                <div class="col-md-12 animate-box" style="background-color: rgb(238,238,118); margin-bottom: 15px; box-shadow: 4px 4px;">
+                    <div class="row" style="padding: 15px 0;">
+                        <div class="col-md-12">
+                            <h4><b>Mantenimiento y soporte técnico</b></h4>
+                            <ul>
+                                <li>Dispensarios y sistema de monitoreo</li>
+                                <li>Imagen e instalaciones</li>
+                                <li>Hidroneumáticos</li>
+                                <li>Motobombas</li>
+                                <li>Asesoría técnica</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 animate-box" style="background-color: rgb(238,238,118); margin-bottom: 15px; box-shadow: 4px 4px;">
+                    <div class="row" style="padding: 15px 0;">
+                        <div class="col-md-12">
+                            <h4><b>Ingeniería y diseño</b></h4>
+                            <ul>
+                                <li>Estudios ambientales y trámites</li>
+                                <li>Topografía</li>
+                                <li>Diseños arquitectónicos</li>
+                                <li>Calculo estructural</li>
+                                <li>Ingeniería mecánica</li>
+                                <li>Ingeniería eléctrica</li>
+                                <li>Diseños hidráulicos-sanitarios</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 animate-box" style="background-color: rgb(238,238,118); margin-bottom: 15px; box-shadow: 4px 4px;">
+                    <div class="row" style="padding: 15px 0;">
+                        <div class="col-md-12">
+                            <h4><b>Construcción</b></h4>
+                            <ul>
+                                <li>Terracerías</li>
+                                <li>Pavimentos</li>
+                                <li>Estructuras</li>
+                                <li>Obra Civil</li>
+                                <li>Instalaciones Electromecánicas</li>
+                                <li>Instalaciones hidráulicas y sanitarias</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 animate-box" style="background-color: rgb(238,238,118); margin-bottom: 15px; box-shadow: 4px 4px;">
+                    <div class="row" style="padding: 15px 0;">
+                        <div class="col-md-12">
+                            <h4><b>Suministros</b></h4>
+                            <ul>
+                                <li>Material mecánico nacional</li>
+                                <li>Material de importación</li>
+                                <li>Refacciones</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
