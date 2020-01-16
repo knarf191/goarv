7d54abaa214d2e1810a370182d065ad75287731b
Frank Reyes Martinez <frank_reyesm@hotmail.com>
2020-01-15 23:37:56 -0600

    work in services section

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -4,64 +4,29 @@
     </div>
     <div id="colorlib-project">
         <div class="container">
+            <h3>NUESTROS SERVICIOS</h3>
+            <hr>
             <div class="row">
-                <h3>NUESTROS SERVICIOS</h3>
-                <hr>
-                <br>
-                <div class="col-md-12 animate-box" style="background-color: rgb(238,238,118); margin-bottom: 15px; box-shadow: 4px 4px;">
-                    <div class="row" style="padding: 15px 0;">
-                        <div class="col-md-12">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-5 service-box animate-box">
                             <h4><b>Mantenimiento y soporte técnico</b></h4>
-                            <ul>
-                                <li>Dispensarios y sistema de monitoreo</li>
-                                <li>Imagen e instalaciones</li>
-                                <li>Hidroneumáticos</li>
-                                <li>Motobombas</li>
-                                <li>Asesoría técnica</li>
-                            </ul>
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-12 animate-box" style="background-color: rgb(238,238,118); margin-bottom: 15px; box-shadow: 4px 4px;">
-                    <div class="row" style="padding: 15px 0;">
-                        <div class="col-md-12">
+                        <div class="col-md-5 service-box animate-box">
                             <h4><b>Ingeniería y diseño</b></h4>
-                            <ul>
-                                <li>Estudios ambientales y trámites</li>
-                                <li>Topografía</li>
-                                <li>Diseños arquitectónicos</li>
-                                <li>Calculo estructural</li>
-                                <li>Ingeniería mecánica</li>
-                                <li>Ingeniería eléctrica</li>
-                                <li>Diseños hidráulicos-sanitarios</li>
-                            </ul>
+
                         </div>
                     </div>
                 </div>
-                <div class="col-md-12 animate-box" style="background-color: rgb(238,238,118); margin-bottom: 15px; box-shadow: 4px 4px;">
-                    <div class="row" style="padding: 15px 0;">
-                        <div class="col-md-12">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-5 service-box animate-box">
                             <h4><b>Construcción</b></h4>
-                            <ul>
-                                <li>Terracerías</li>
-                                <li>Pavimentos</li>
-                                <li>Estructuras</li>
-                                <li>Obra Civil</li>
-                                <li>Instalaciones Electromecánicas</li>
-                                <li>Instalaciones hidráulicas y sanitarias</li>
-                            </ul>
+
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-12 animate-box" style="background-color: rgb(238,238,118); margin-bottom: 15px; box-shadow: 4px 4px;">
-                    <div class="row" style="padding: 15px 0;">
-                        <div class="col-md-12">
+                        <div class="col-md-5 service-box animate-box">
                             <h4><b>Suministros</b></h4>
-                            <ul>
-                                <li>Material mecánico nacional</li>
-                                <li>Material de importación</li>
-                                <li>Refacciones</li>
-                            </ul>
+
                         </div>
                     </div>
                 </div>
